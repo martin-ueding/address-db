9c79d2adfeda41e27fef0e00f38253e5dca8be47
Martin Ueding <von.bzr@martin-ueding.de>
2010-10-22 22:00:42 +0200

    fixed delete

--- adressen/pages/person_delete.inc.php
+++ adressen/pages/person_delete.inc.php
@@ -3,16 +3,17 @@ if (!empty($_GET['id'])) {
 	if ($_GET['okay'] == 'ja') {
 		delete_person_id($_GET['id']);
 
-		echo 'Die Person wurde gel&ouml;scht. <a href="main.php">Zur&uuml;ck</a>';
+		echo 'Die Person wurde gel&ouml;scht. <a href="index.php">Zur&uuml;ck</a>';
 	}
 
 	else {
-		echo '<form action="person_loeschen.php" method="get">';
-		echo '<input type="hidden" name="id" value="'.$_GET['id'].'" />';
+		echo '<form action="index.php" method="GET">';
 		echo '<input type="checkbox" name="okay" value="ja" />';
+		echo '<input type="hidden" name="mode" value="person_delete" />';
+		echo '<input type="hidden" name="id" value="'.$_GET['id'].'" />';
 		echo '<input type="submit" value="L&Ouml;SCHEN" />';
 		echo '</form>';
-		echo '<a href="personenanzeige.php?id='.$_GET['id'].'">Abbrechen</a>';
+		echo '<a href="index.php?mode=person_display&id='.$_GET['id'].'">Abbrechen</a>';
 	}
 }
 
