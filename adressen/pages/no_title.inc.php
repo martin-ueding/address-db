6c9ff867618814e949a526070100c3bf43b45fb9
Martin Ueding <dev@martin-ueding.de>
2010-11-21 20:22:15 +0100

    added alternating colors in no_title view. added page title as well

--- adressen/pages/no_title.inc.php
+++ adressen/pages/no_title.inc.php
@@ -1,4 +1,5 @@
 <?PHP
+echo 'Personen ohne Anrede:<br /><br />';
 
 $titel = $_GET["titel"];
 
@@ -7,8 +8,9 @@ if ($_SESSION['f'] != 0)
 else
 	$sql = 'SELECT * FROM ad_per WHERE anrede_r=1';
 $erg = mysql_query($sql);
+$i = 0;
 while ($l = mysql_fetch_assoc($erg)) {
-	$daten[] = '<tr><td><a href="?mode=person_display&id='.$l['p_id'].'">&raquo;</a></td><td><a href="?mode=person_display&id='.$l['p_id'].'">'.$l['vorname'].'</a></td><td><a href="?mode=person_display&id='.$l['p_id'].'">'.$l['nachname'].'</a></td></tr>';
+	$daten[] = '<tr class="'.($i++ % 2 == 0 ? 'hell' : 'dunkel').'"><td><a href="?mode=person_display&id='.$l['p_id'].'">&raquo;</a></td><td><a href="?mode=person_display&id='.$l['p_id'].'">'.$l['vorname'].'</a></td><td><a href="?mode=person_display&id='.$l['p_id'].'">'.$l['nachname'].'</a></td></tr>';
 }
 
 /* Daten anzeigen */
