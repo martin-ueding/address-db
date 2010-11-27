a56eb7723fe27163fe60609cd38d47c3caaa0256
Martin Ueding <dev@martin-ueding.de>
2010-11-27 16:30:21 +0100

    changed $l to $person_loop in person_display to avoid overwriting the person's data with some other SQL queries

--- adressen/pages/person_display.inc.php
+++ adressen/pages/person_display.inc.php
@@ -3,7 +3,7 @@ if (empty($id)) {
 	$id = $_GET["id"];
 }
 $erg = select_person_alles($id);
-$l = mysql_fetch_assoc($erg);
+$person_loop = mysql_fetch_assoc($erg);
 $mugshot_path = '_mugshots/per'.$id.'.jpg';
 if (file_exists($mugshot_path)) {
 	$bilddaten = getimagesize('_mugshots/per'.$id.'.jpg');
@@ -20,33 +20,33 @@ echo '<table id="name">';
 echo '<tr>';
 echo '<td class="links">Anrede:</td>';
 echo '<td class="rechts">';
-if ($l['anrede'] != "-")
-	echo ' '.$l['anrede'];
-if ($l['prafix'] != "-")
-	echo ' '.$l['prafix'];
+if ($person_loop['anrede'] != "-")
+	echo ' '.$person_loop['anrede'];
+if ($person_loop['prafix'] != "-")
+	echo ' '.$person_loop['prafix'];
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<td class="links">Name:</td>';
 echo '<td>';
-echo '<b>'.$l['vorname'].'</b>';
-if (!empty($l['mittelname']))
-	echo ' '.$l['mittelname'];
-echo ' <b>'.$l['nachname'].'</b>';
-if ($l['suffix'] != "-")
-	echo ' ('.$l['suffix'].')';
-if (!empty($l['geburtsname'])) {
-	echo ', geborene'.(empty($l['anrede']) ? '(r)' : ($l['anrede'] == "Herr" ? 'r' : '')).' '.$l['geburtsname'];
+echo '<b>'.$person_loop['vorname'].'</b>';
+if (!empty($person_loop['mittelname']))
+	echo ' '.$person_loop['mittelname'];
+echo ' <b>'.$person_loop['nachname'].'</b>';
+if ($person_loop['suffix'] != "-")
+	echo ' ('.$person_loop['suffix'].')';
+if (!empty($person_loop['geburtsname'])) {
+	echo ', geborene'.(empty($person_loop['anrede']) ? '(r)' : ($person_loop['anrede'] == "Herr" ? 'r' : '')).' '.$person_loop['geburtsname'];
 }
 echo '</td>';
 echo '</tr>';
-if (!empty($l['geb_t'])) {	
+if (!empty($person_loop['geb_t'])) {	
 	echo '<tr>';
 	echo '<td class="links">Geburtstag:</td>';
-	echo '<td>'.$l['geb_t'].'.'.$l['geb_m'].'.';
-	if ($l['geb_j'] > 1500) 
-		echo $l['geb_j'].' &nbsp;&nbsp; (heute '.alter($l['geb_t'],$l['geb_m'],$l['geb_j']).' Jahre alt)&nbsp;&nbsp; ('.sternzeichen ($l['geb_t'], $l['geb_m']).')';
+	echo '<td>'.$person_loop['geb_t'].'.'.$person_loop['geb_m'].'.';
+	if ($person_loop['geb_j'] > 1500) 
+		echo $person_loop['geb_j'].' &nbsp;&nbsp; (heute '.alter($person_loop['geb_t'],$person_loop['geb_m'],$person_loop['geb_j']).' Jahre alt)&nbsp;&nbsp; ('.sternzeichen ($person_loop['geb_t'], $person_loop['geb_m']).')';
 	echo '</td>';
 	echo '</tr>';
 	}
@@ -56,56 +56,56 @@ echo '<div class="pers_titel">';
 echo '&nbsp;&nbsp;Adresse:';
 echo '</div>';
 echo '<table id="adresse">';
-if ($l['adresse_r'] != 1) {
+if ($person_loop['adresse_r'] != 1) {
 	echo '<tr>';
 	echo '<td class="links">Adresse:</td>';
 	echo '<td class="rechts">';
-	echo $l['strasse'];
+	echo $person_loop['strasse'];
 	echo ', ';
-	echo $l['plz'].' '.$l['ortsname'];
-	echo ' ('.$l['land'].')';
+	echo $person_loop['plz'].' '.$person_loop['ortsname'];
+	echo ' ('.$person_loop['land'].')';
 	echo '<br />';
 	echo 'Online-Karte zeigen: ';
-	echo '<a href="http://maps.google.de/maps?f=q&hl=de&q='.urlencode($l['strasse'].', '.$l['ortsname'].', '.$l['plz'].', '.$l['land']).'" target="_blank">&raquo;GoogleMaps</a>';
-	if ($l['land'] == 'Deutschland') {
+	echo '<a href="http://maps.google.de/maps?f=q&hl=de&q='.urlencode($person_loop['strasse'].', '.$person_loop['ortsname'].', '.$person_loop['plz'].', '.$person_loop['land']).'" target="_blank">&raquo;GoogleMaps</a>';
+	if ($person_loop['land'] == 'Deutschland') {
 		echo ' &nbsp; ';
-		echo '<a href="http://www.mapquest.com/maps/map.adp?country=de&address='.str_replace(' ', '+', $l['strasse']).'&city='.str_replace(' ', '+', $l['ortsname']).'&zip='.$l['plz'].'" target="_blank">&raquo;MapQuest</a>';
+		echo '<a href="http://www.mapquest.com/maps/map.adp?country=de&address='.str_replace(' ', '+', $person_loop['strasse']).'&city='.str_replace(' ', '+', $person_loop['ortsname']).'&zip='.$person_loop['plz'].'" target="_blank">&raquo;MapQuest</a>';
 	}
 	echo '</td>';
 	echo '</tr>';
 }
-if (!empty($l['ftel_privat'])) {		
+if (!empty($person_loop['ftel_privat'])) {		
 	echo '<tr>';
 	echo '<td class="links">Telefon Privat:</td>';
-	echo '<td>'.select_vw_id($l['fvw_privat_r']).'-'.$l['ftel_privat'].' '.skplnk(select_vw_id($l['fvw_privat_r']).$l['ftel_privat']).'</td>';
+	echo '<td>'.select_vw_id($person_loop['fvw_privat_r']).'-'.$person_loop['ftel_privat'].' '.skplnk(select_vw_id($person_loop['fvw_privat_r']).$person_loop['ftel_privat']).'</td>';
 	echo '</tr>';
 }
 
-if (!empty($l['ftel_arbeit'])) {	
+if (!empty($person_loop['ftel_arbeit'])) {	
 	echo '<tr>';
 	echo '<td class="links">Telefon Arbeit:</td>';
-	echo '<td>'.select_vw_id($l['fvw_arbeit_r']).'-'.$l['ftel_arbeit'].' '.skplnk(select_vw_id($l['fvw_arbeit_r']).$l['ftel_arbeit']).'</td>';
+	echo '<td>'.select_vw_id($person_loop['fvw_arbeit_r']).'-'.$person_loop['ftel_arbeit'].' '.skplnk(select_vw_id($person_loop['fvw_arbeit_r']).$person_loop['ftel_arbeit']).'</td>';
 	echo '</tr>';
 }
 
-if (!empty($l['ftel_mobil'])) {	
+if (!empty($person_loop['ftel_mobil'])) {	
 	echo '<tr>';
-	echo '<td class="links">Handy: <i>'.handybetreiber(select_vw_id($l['vw_mobil_r'])).'</i></td>';
-	echo '<td>'.select_vw_id($l['fvw_mobil_r']).'-'.$l['ftel_mobil'].' '.skplnk(select_vw_id($l['fvw_mobil_r']).$l['ftel_mobil']).'</td>';
+	echo '<td class="links">Handy: <i>'.handybetreiber(select_vw_id($person_loop['vw_mobil_r'])).'</i></td>';
+	echo '<td>'.select_vw_id($person_loop['fvw_mobil_r']).'-'.$person_loop['ftel_mobil'].' '.skplnk(select_vw_id($person_loop['fvw_mobil_r']).$person_loop['ftel_mobil']).'</td>';
 	echo '</tr>';
 }
 
-if (!empty($l['ftel_fax'])) {		
+if (!empty($person_loop['ftel_fax'])) {		
 	echo '<tr>';
 	echo '<td class="links">Fax:</td>';
-	echo '<td>'.select_vw_id($l['fvw_fax_r']).'-'.$l['ftel_fax'].'</td>';
+	echo '<td>'.select_vw_id($person_loop['fvw_fax_r']).'-'.$person_loop['ftel_fax'].'</td>';
 	echo '</tr>';
 }
 
-if (!empty($l['ftel_aux'])) {	
+if (!empty($person_loop['ftel_aux'])) {	
 	echo '<tr>';
 	echo '<td class="links">Telefon Sonstiges:</td>';
-	echo '<td>'.select_vw_id($l['fvw_aux_r']).'-'.$l['ftel_aux'].' '.skplnk(select_vw_id($l['fvw_aux_r']).$l['ftel_aux']).'</td>';
+	echo '<td>'.select_vw_id($person_loop['fvw_aux_r']).'-'.$person_loop['ftel_aux'].' '.skplnk(select_vw_id($person_loop['fvw_aux_r']).$person_loop['ftel_aux']).'</td>';
 	echo '</tr>';
 }
 echo '</table>';
@@ -114,38 +114,38 @@ echo '<div class="pers_titel">';
 echo '&nbsp;&nbsp;Telefon:';
 echo '</div>';
 echo '<table id="telefon">';
-if (!empty($l['tel_privat'])) {		
+if (!empty($person_loop['tel_privat'])) {		
 	echo '<tr>';
 	echo '<td class="links">Privat:</td>';
-	echo '<td class="rechts">'.select_vw_id($l['vw_privat_r']).'-'.$l['tel_privat'].' '.skplnk(select_vw_id($l['vw_privat_r']).$l['tel_privat']).'</td>';
+	echo '<td class="rechts">'.select_vw_id($person_loop['vw_privat_r']).'-'.$person_loop['tel_privat'].' '.skplnk(select_vw_id($person_loop['vw_privat_r']).$person_loop['tel_privat']).'</td>';
 	echo '</tr>';
 }
 
-if (!empty($l['tel_arbeit'])) {	
+if (!empty($person_loop['tel_arbeit'])) {	
 	echo '<tr>';
 	echo '<td class="links">Arbeit:</td>';
-	echo '<td>'.select_vw_id($l['vw_arbeit_r']).'-'.$l['tel_arbeit'].' '.skplnk(select_vw_id($l['vw_arbeit_r']).$l['tel_arbeit']).'</td>';
+	echo '<td>'.select_vw_id($person_loop['vw_arbeit_r']).'-'.$person_loop['tel_arbeit'].' '.skplnk(select_vw_id($person_loop['vw_arbeit_r']).$person_loop['tel_arbeit']).'</td>';
 	echo '</tr>';
 }
 
-if (!empty($l['tel_mobil'])) {	
+if (!empty($person_loop['tel_mobil'])) {	
 	echo '<tr>';
-	echo '<td class="links">Handy: <i>'.handybetreiber(select_vw_id($l['vw_mobil_r'])).'</i></td>';
-	echo '<td>'.select_vw_id($l['vw_mobil_r']).'-'.$l['tel_mobil'].' '.skplnk(select_vw_id($l['vw_mobil_r']).$l['tel_mobil']).'</td>';
+	echo '<td class="links">Handy: <i>'.handybetreiber(select_vw_id($person_loop['vw_mobil_r'])).'</i></td>';
+	echo '<td>'.select_vw_id($person_loop['vw_mobil_r']).'-'.$person_loop['tel_mobil'].' '.skplnk(select_vw_id($person_loop['vw_mobil_r']).$person_loop['tel_mobil']).'</td>';
 	echo '</tr>';
 }
 		
-if (!empty($l['tel_fax'])) {		
+if (!empty($person_loop['tel_fax'])) {		
 	echo '<tr>';
 	echo '<td class="links">Fax:</td>';
-	echo '<td>'.select_vw_id($l['vw_fax_r']).'-'.$l['tel_fax'].'</td>';
+	echo '<td>'.select_vw_id($person_loop['vw_fax_r']).'-'.$person_loop['tel_fax'].'</td>';
 	echo '</tr>';
 }
 
-if (!empty($l['tel_aux'])) {	
+if (!empty($person_loop['tel_aux'])) {	
 	echo '<tr>';
 	echo '<td class="links">Sonstiges:</td>';
-	echo '<td>'.select_vw_id($l['vw_aux_r']).'-'.$l['tel_aux'].' '.skplnk(select_vw_id($l['vw_aux_r']).$l['tel_aux']).'</td>';
+	echo '<td>'.select_vw_id($person_loop['vw_aux_r']).'-'.$person_loop['tel_aux'].' '.skplnk(select_vw_id($person_loop['vw_aux_r']).$person_loop['tel_aux']).'</td>';
 	echo '</tr>';
 }
 echo '</table>';
@@ -155,107 +155,102 @@ echo '<div class="pers_titel">';
 echo '&nbsp;&nbsp;Internet:';
 echo '</div>';
 echo '<table id="online">';
-if (!empty($l['email_privat'])) {	
+if (!empty($person_loop['email_privat'])) {	
 	echo '<tr>';
 	echo '<td class="links">Email Privat:</td>';
 	echo '<td class="icon"><img src="gfx/10/email10.png" width="10" height="10" /></td>';
-	echo '<td class="rechts"><a href="mailto:'.$l['email_privat'].'">'.$l['email_privat'].'</a></td>';
+	echo '<td class="rechts"><a href="mailto:'.$person_loop['email_privat'].'">'.$person_loop['email_privat'].'</a></td>';
 	echo '</tr>';
 	$emailadresse_vorhanden = true;
 }
-if (!empty($l['email_arbeit'])) {	
+if (!empty($person_loop['email_arbeit'])) {	
 	echo '<tr>';
 	echo '<td class="links">Email Arbeit:</td>';
 	echo '<td class="icon"><img src="gfx/10/email10.png" width="10" height="10" /></td>';
-	echo '<td><a href="mailto:'.$l['email_arbeit'].'">'.$l['email_arbeit'].'</a></td>';
+	echo '<td><a href="mailto:'.$person_loop['email_arbeit'].'">'.$person_loop['email_arbeit'].'</a></td>';
 	echo '</tr>';
 	$emailadresse_vorhanden = true;
 }
-if (!empty($l['email_aux'])) {	
+if (!empty($person_loop['email_aux'])) {	
 	echo '<tr>';
 	echo '<td class="links">Email Sonstiges:</td>';
 	echo '<td class="icon"><img src="gfx/10/email10.png" width="10" height="10" /></td>';
-	echo '<td><a href="mailto:'.$l['email_aux'].'">'.$l['email_aux'].'</a></td>';
+	echo '<td><a href="mailto:'.$person_loop['email_aux'].'">'.$person_loop['email_aux'].'</a></td>';
 	echo '</tr>';
 	$emailadresse_vorhanden = true;
 }
-if (!empty($l['hp1'])) {	
+if (!empty($person_loop['hp1'])) {	
 	echo '<tr>';
 	echo '<td class="links">Homepage 1:</td>';
 	echo '<td class="icon"><img src="gfx/10/www10.png" width="10" height="10" /></td>';
-	echo '<td><a href="http://'.$l['hp1'].'" target="_blank">'.$l['hp1'].'</a></td>';
+	echo '<td><a href="http://'.$person_loop['hp1'].'" target="_blank">'.$person_loop['hp1'].'</a></td>';
 	echo '</tr>';
 }
-if (!empty($l['hp2'])) {	
+if (!empty($person_loop['hp2'])) {	
 	echo '<tr>';
 	echo '<td class="links">Homepage 2:</td>';
 	echo '<td class="icon"><img src="gfx/10/www10.png" width="10" height="10" /></td>';
-	echo '<td><a href="http://'.$l['hp2'].'" target="_blank">'.$l['hp2'].'</a></td>';
+	echo '<td><a href="http://'.$person_loop['hp2'].'" target="_blank">'.$person_loop['hp2'].'</a></td>';
 	echo '</tr>';
 }
 		
 
-if (!empty($l['chat_aim'])) {	
+if (!empty($person_loop['chat_aim'])) {	
 	echo '<tr>';
 	echo '<td class="links">Chat AIM:</td>';
 	echo '<td class="icon"><img src="gfx/10/aim10.png" width="10" height="10" /></td>';
-	echo '<td><a href="AIM://'.$l['chat_aim'].'">'.$l['chat_aim'].'</a></td>';
+	echo '<td><a href="AIM://'.$person_loop['chat_aim'].'">'.$person_loop['chat_aim'].'</a></td>';
 	echo '</tr>';
 }
-if (!empty($l['chat_msn'])) {	
+if (!empty($person_loop['chat_msn'])) {	
 	echo '<tr>';
 	echo '<td class="links">Chat MSN:</td>';
 	echo '<td class="icon"><img src="gfx/10/msn10.png" width="10" height="10" /></td>';
-	echo '<td><a href="MSN://'.$l['chat_msn'].'">'.$l['chat_msn'].'</a></td>';
+	echo '<td><a href="MSN://'.$person_loop['chat_msn'].'">'.$person_loop['chat_msn'].'</a></td>';
 	echo '</tr>';
 }
-if (!empty($l['chat_icq'])) {	
+if (!empty($person_loop['chat_icq'])) {	
 	echo '<tr>';
 	echo '<td class="links">Chat ICQ:</td>';
 	echo '<td class="icon"><img src="gfx/10/icq10.png" width="10" height="10" /></td>';
-	echo '<td><a href="ICQ://'.$l['chat_icq'].'">#'.$l['chat_icq'].'</a> &nbsp; <a href="http://people.icq.com/'.$l['chat_icq'].'" target="_blank">&raquo; Profil</a></td>';
+	echo '<td><a href="ICQ://'.$person_loop['chat_icq'].'">#'.$person_loop['chat_icq'].'</a> &nbsp; <a href="http://people.icq.com/'.$person_loop['chat_icq'].'" target="_blank">&raquo; Profil</a></td>';
 	echo '</tr>';
 }
-if (!empty($l['chat_yim'])) {	
+if (!empty($person_loop['chat_yim'])) {	
 	echo '<tr>';
 	echo '<td class="links">Chat Yahoo:</td>';
 	echo '<td class="icon"><img src="gfx/10/yim10.png" width="10" height="10" /></td>';
-	echo '<td><a href="Yahoo://'.$l['chat_yim'].'">'.$l['chat_yim'].'</a></td>';
+	echo '<td><a href="Yahoo://'.$person_loop['chat_yim'].'">'.$person_loop['chat_yim'].'</a></td>';
 	echo '</tr>';
 }
-if (!empty($l['chat_skype'])) {	
+if (!empty($person_loop['chat_skype'])) {	
 	echo '<tr>';
 	echo '<td class="links">Chat Skype:</td>';
 	echo '<td class="icon"><img src="gfx/10/skype10.png" width="10" height="10" /></td>';
-	echo '<td><a href="Callto://'.$l['chat_skype'].'">'.$l['chat_skype'].'</a></td>';
+	echo '<td><a href="Callto://'.$person_loop['chat_skype'].'">'.$person_loop['chat_skype'].'</a></td>';
 	echo '</tr>';
 }
-if (!empty($l['chat_aux'])) {	
+if (!empty($person_loop['chat_aux'])) {	
 	echo '<tr>';
 	echo '<td class="links">Chat Aux:</td>';
 	echo '<td class="icon">&nbsp;</td>';
-	echo '<td>'.$l['chat_aux'].'</td>';
+	echo '<td>'.$person_loop['chat_aux'].'</td>';
 	echo '</tr>';
 }
 		
 		
 echo '</table>';
 
-if (!empty($l['pnotizen'])) {
+if (!empty($person_loop['pnotizen'])) {
 	echo '<div class="pers_titel">';
 	echo '&nbsp;&nbsp;Notizen:';
 	echo '</div>';
 	echo '<table id="notizen">';
 	echo '<tr>';
-	echo '<td>'.nl2br($l['pnotizen']).'</td>';
+	echo '<td>'.nl2br($person_loop['pnotizen']).'</td>';
 	echo '</tr>';
 	echo '</table>';
 }
-
-// TODO clean this up by using $person_loop as variable name
-$check = $l['last_check'];
-$last_send = $l['last_send'];
-$last_edit = $l['last_edit'];
 
 //		Gruppen
 echo '<div class="pers_titel">';
@@ -301,20 +296,20 @@ echo '<td>';
 $anzahl_level = 6;
 $veraltet_nach = 365;
 
-$letzter_check_vor = round((time()-$check)/3600/24);
+$letzter_check_vor = round((time()-$person_loop['last_check'])/3600/24);
 
 $aktuell_level = round($anzahl_level*($veraltet_nach-$letzter_check_vor)/$veraltet_nach);
 
 echo '<div style="padding: 5px;">';
 
-if ($check == 0)
+if ($person_loop['last_check'] == 0)
 	$aktuell_level = 0;
 else {
 	$aktuell_level = round($anzahl_level*($veraltet_nach-$letzter_check_vor)/$veraltet_nach);
 }
 
 for ($i = $anzahl_level-1; $i >= 0 ; $i--) {
-	echo '<img src="gfx/balken_'.($i < $aktuell_level ? 'aktiv' : 'inaktiv').'.png" title="Zuletzt '.intelligent_date($check).' (vor '.$letzter_check_vor.' Tagen) überprüft." />';
+	echo '<img src="gfx/balken_'.($i < $aktuell_level ? 'aktiv' : 'inaktiv').'.png" title="Zuletzt '.intelligent_date($person_loop['last_check']).' (vor '.$letzter_check_vor.' Tagen) überprüft." />';
 }
 	
 echo '<div>';
@@ -329,18 +324,18 @@ if ($emailadresse_vorhanden) {
 	echo '<td>';
 
 	echo '<a href="index.php?mode=verification_email&id='.$id.'">&raquo; Überprüfungsemail senden</a>';
-	if ($last_send != 0) {
-		echo ' (letzte vom '.date($date_format, $last_send).')';
+	if ($person_loop['last_send'] != 0) {
+		echo ' (letzte vom '.date($date_format, $person_loop['last_send']).')';
 	}
-	if ($check < $last_send) {
-		echo '<br />Mail '.intelligent_date($last_send),' gesendet, Überprüfung ausstehend';
+	if ($person_loop['last_check'] < $person_loop['last_send']) {
+		echo '<br />Mail '.intelligent_date($person_loop['last_send']),' gesendet, Überprüfung ausstehend';
 	}
 	echo '</td>';
 	echo '</tr>';
 }
 echo '<tr>';
 echo '<td class="links">Zuletzt editiert:</td>';
-echo '<td>'.intelligent_date($last_edit).'</td>';
+echo '<td>'.intelligent_date($person_loop['last_edit']).'</td>';
 echo '</td>';
 			
 echo '</table>';
