d9dc63760b117f5259be10a65e288814b6145e43
Martin Ueding <dev@martin-ueding.de>
2010-11-28 13:03:42 +0100

    fixed: after checking a person the data is not reloaded

--- adressen/engines/person_checked.inc.php
+++ adressen/engines/person_checked.inc.php
@@ -3,5 +3,8 @@ $sql = 'UPDATE ad_per SET last_check='.time().' WHERE p_id='.($id).';';
 mysql_query($sql);
 $msgs[] = 'Person wurde aktualisiert.';
 
+// update the data for the person
+$person_loop['last_edit'] = time();
+
 $mode= 'person_display';
 ?>
