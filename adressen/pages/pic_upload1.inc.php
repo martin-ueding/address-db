de6380938981706a34a46d4d5fad74f9e8d9361a
Martin Ueding <dev@martin-ueding.de>
2011-01-06 21:20:34 +0100

    i18n

--- adressen/pages/pic_upload1.inc.php
+++ adressen/pages/pic_upload1.inc.php
@@ -4,7 +4,7 @@
 
 <form enctype="multipart/form-data" action="index.php?mode=pic_upload1&fertig=ja" method="post"> 
 <input type="file" name="file">
-<input type="submit" value="Hochladen">
+<input type="submit" value="<?PHP echo _('upload'); ?>">
 <?PHP
 echo '<input type="hidden" name="id" value="'.$_GET['id'].'" />';
 ?>
--- adressen/pages/verification_email.inc.php
+++ adressen/pages/verification_email.inc.php
@@ -1,8 +1,8 @@
 <?PHP
 if ($_GET['sicher'] != 'ja') {
-	echo 'M&ouml;chten Sie wirklich eine &Uuml;berpr&uuml;fungsmail an <em>'.$person_loop['vorname'].' '.$person_loop['nachname'].'</em> senden?<br /><br />';
-	echo '<a href="index.php?mode=verification_email&id='.$id.'&sicher=ja">Sicher</a>';
+	printf(_('Do you really want to send a verification mail to %s?'), '<em>'.$person_loop['vorname'].' '.$person_loop['nachname'].'</em>');
+	echo '<a href="index.php?mode=verification_email&id='.$id.'&sicher=ja">'._('sure').'</a>';
 	echo '<br /><br />';
-	echo '<a href="index.php?mode=person_display&id='.$id.'">NICHT SENDEN!</a>';
+	echo '<a href="index.php?mode=person_display&id='.$id.'">'._('Do not send!').'</a>';
 }
 ?>
