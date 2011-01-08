033420b302f0df3e67c54688d19c888b5cf2a96a
Martin Ueding <dev@martin-ueding.de>
2011-01-08 23:55:28 +0100

    added missing line breaks

--- adressen/pages/verification_email.inc.php
+++ adressen/pages/verification_email.inc.php
@@ -1,6 +1,7 @@
 <?PHP
 if ($_GET['sicher'] != 'ja') {
 	printf(_('Do you really want to send a verification mail to %s?'), '<em>'.$person_loop['vorname'].' '.$person_loop['nachname'].'</em>');
+	echo '<br /><br />';
 	echo '<a href="index.php?mode=verification_email&id='.$id.'&sicher=ja">'._('sure').'</a>';
 	echo '<br /><br />';
 	echo '<a href="index.php?mode=person_display&id='.$id.'">'._('Do not send!').'</a>';
