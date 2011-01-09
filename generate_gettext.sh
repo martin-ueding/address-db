67bc391f5e180a110bad2c50c46ae561d9dbbfb4
Martin Ueding <dev@martin-ueding.de>
2011-01-09 12:23:21 +0100

    now updating the de/main.mo file

--- generate_gettext.sh
+++ generate_gettext.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 xgettext --language=PHP --from-code=UTF-8 -keywword=_ -o locale/main.pot adressen/*.php adressen/*/*.php adressen_helper/*.php
-#
-#msgfmt -o locale/de_DE/LC_MESSAGES/main.mo locale/de.po
+
+msgfmt -o locale/de_DE/LC_MESSAGES/main.mo locale/de.po
 
