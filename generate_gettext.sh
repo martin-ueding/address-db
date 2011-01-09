ffe5d2fcccddffc120445a20836d35bee9604790
Martin Ueding <dev@martin-ueding.de>
2011-01-09 12:30:58 +0100

    typo in generating file

--- generate_gettext.sh
+++ generate_gettext.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-xgettext --language=PHP --from-code=UTF-8 -keywword=_ -o locale/main.pot adressen/*.php adressen/*/*.php adressen_helper/*.php
+xgettext --language=PHP --from-code=UTF-8 -keyword=_ -o locale/main.pot adressen/*.php adressen/*/*.php adressen_helper/*.php
 
 msgfmt -o locale/de_DE/LC_MESSAGES/main.mo locale/de.po
 
