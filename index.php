529a207b21eb5ca5bec66f0a5c18ff6e46d92832
Martin Ueding <dev@martin-ueding.de>
2011-02-20 17:00:55 +0100

    added welcoming page

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
 <?PHP
 $f = (int)$_GET['f'];
 // since this is not the main directory for displaying, one is forwareded.
+
+
+echo '<!doctype html>';
+echo '<html>';
+echo '<head>';
+echo '<meta http-equiv="refresh" content="0; ';
+
 if ($f == 0)
-	header('location:adressen/index.php');
+	echo 'adressen/index.php';
 else
-	header('location:adressen/index.php?f='.$f);
+	echo 'adressen/index.php?f='.$f;
+
+echo '">';
+echo '<style type="text/css">
+#welcome {
+	font-family: ubuntu, verdana, sans-serif;
+color: #555;
+	font-size: 70px;
+margin: 40px auto 0px auto;
+width: 600px;
+}
+</style>';
+
+echo '<title></title>';
+echo '</head>';
+echo '<body>';
+echo '<div id="welcome">Adressdatenbank</div>';
+echo '</body>';
+echo '</html>';
 ?>
