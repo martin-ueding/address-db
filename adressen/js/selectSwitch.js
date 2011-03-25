7564ea2c25a0e9dd634a78ba295bef7742e589ea
Martin Ueding <dev@martin-ueding.de>
2011-03-25 20:04:17 +0100

    added animations

--- adressen/js/selectSwitch.js
+++ adressen/js/selectSwitch.js
@@ -5,17 +5,17 @@
 $(initSwitchFields);
 
 function initSwitchFields() {
-	$("input.manual_area_code").hide(1000).parent().children("input.manual_area_code").before("<img src='gfx/agt_reload.png' class='switch_fields' title='switch between a select and a manual input' />");
+	$("input.manual_area_code").hide(0).parent().children("input.manual_area_code").before("<img src='gfx/agt_reload.png' class='switch_fields' title='switch between a select and a manual input' />");
 
 	$("img.switch_fields").click(function () {
 		if ($(this).parent().children("select").css("display") == "none") {
-			$(this).parent().children("input.manual_area_code").hide().val("");
-			$(this).parent().children("select").show();
+			$(this).parent().children("input.manual_area_code").hide(100, function() {$(this).parent().children("select").show(100);}).val("");
+			
 			
 		}
 		else {
-			$(this).parent().children("input.manual_area_code").show();
-			$(this).parent().children("select").hide();
+			$(this).parent().children("select").hide(100, function () {$(this).parent().children("input.manual_area_code").show(100);});
+			
 		}
 		});
 }
