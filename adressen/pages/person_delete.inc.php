c2cfd549520370d1f97ae4d9ea43a3a508d926cf
Martin Ueding <dev@martin-ueding.de>
2011-04-11 13:21:49 +0200

    added a warning when deleting a person that is not only connected to oneself

--- adressen/pages/person_delete.inc.php
+++ adressen/pages/person_delete.inc.php
@@ -4,6 +4,25 @@
 echo '<h1>'._('delete an entry').'</h1>';
 if (!empty($id)) {
 	printf(_('Do you really want to delete the entry %s?'), '<em>'.$person_loop['vorname'].' '.$person_loop['nachname'].'</em>');
+
+	// check for multiple associations
+	$ass_sql = 'SELECT * FROM ad_flinks LEFT JOIN ad_fmg ON fmg_id=fmg_lr WHERE person_lr='.(int)$id.' ORDER BY fmg;';
+	$ass_erg = mysql_query($ass_sql);
+	while ($ass_l = mysql_fetch_assoc($ass_erg)) {
+		$association_ids[] = $ass_l['fmg_id'];
+		$association_names[] = $ass_l['fmg'];
+	}
+	if (count($association_ids) >= 2) {
+		echo '<br />';
+		echo '<br />';
+		echo _('Warning:').' '.sprintf(_('This entry is connected to %s. Consider just removing your association with it.'), implode(', ', $association_names));
+	}
+	else if (count($association_ids) == 1 && $association_ids[0] != $_SESSION['f']) {
+		echo '<br />';
+		echo '<br />';
+		echo _('Warning:').' '.sprintf(_('This entry is connected to %s. Are you sure that you can delete this entry?'), $association_names[0]);
+	}
+
 	echo '<br />';
 	echo '<br />';
 	echo '<a href="index.php?mode=person_delete2&id='.$id.'&back='.urlencode($_GET['back']).'">'._('Sure, delete that!').'</a>';
