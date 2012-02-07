2b70e0e4f9b24e81ceb6cc43c06dc5848cc2ec66
Martin Ueding <dev@martin-ueding.de>
2012-02-07 12:39:04 +0100

    Use filter for no_association

--- adressen/pages/no_association.inc.php
+++ adressen/pages/no_association.inc.php
@@ -1,22 +1,25 @@
 <?PHP
-// Copyright (c) 2011 Martin Ueding <dev@martin-ueding.de>
+// Copyright (c) 2011-2012 Martin Ueding <dev@martin-ueding.de>
+
+require_once('../helpers/Filter.php');
 
 echo '<h1>'._('entries without an association').'</h1>';
 $from_with_get = 'mode=no_association';
 
-$titel = $_GET["titel"];
-
-$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON person_lr=p_id WHERE person_lr IS NULL ORDER BY nachname, vorname;';
+$filter = new Filter(0, $_SESSION['g']);
+$filter->add_where('ad_flinks.person_lr IS NULL');
+$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON person_lr=p_id '.$filter->join().' WHERE '.$filter->where().' ORDER BY nachname, vorname;';
 
 $erg = mysql_query($sql);
 $i = 0;
 while ($l = mysql_fetch_assoc($erg)) {
+	# TODO Use CSS for zebra.
 	$daten[] = '<tr class="'.($i++ % 2 == 0 ? 'hell' : 'dunkel').'"><td><a href="?mode=person_display&id='.$l['p_id'].'&back='.urlencode($from_with_get).'">&raquo;</a></td><td><a href="?mode=person_display&id='.$l['p_id'].'&back='.urlencode($from_with_get).'">'.$l['vorname'].'</a></td><td><a href="?mode=person_display&id='.$l['p_id'].'&back='.urlencode($from_with_get).'">'.$l['nachname'].'</a></td></tr>';
 }
 
 /* Daten anzeigen */
 
-if (count($daten) > 0) {	
+if (isset($daten) && count($daten) > 0) {
 	echo '<table id="liste">';
 
 	foreach($daten as $zeile)
