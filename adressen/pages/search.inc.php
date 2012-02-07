4fb7af996731f8e4cf21231575d0cff7a6264ccc
Martin Ueding <dev@martin-ueding.de>
2012-02-07 15:05:22 +0100

    Use filter in search

--- adressen/pages/search.inc.php
+++ adressen/pages/search.inc.php
@@ -2,6 +2,7 @@
 // Copyright (c) 2011-2012 Martin Ueding <dev@martin-ueding.de>
 
 require_once('../helper/Table.php');
+require_once('../helper/Filter.php');
 
 echo '<h1>'._('search').'</h1>';
 
@@ -10,9 +11,16 @@ $from_with_get = 'mode=search&suche='.$suche;
 
 $treffer = false;
 
-function display_name_list($sql, $title) {
+function display_name_list($filter, $title) {
+	$filter->add_join('LEFT JOIN ad_adressen ON adresse_r=ad_id', true);
+	$filter->add_join('LEFT JOIN ad_orte ON ort_r=o_id');
+	$filter->add_join('LEFT JOIN ad_plz ON plz_r=plz_id');
+	$filter->add_join('LEFT JOIN ad_laender ON land_r=l_id');
+
+	$sql = 'SELECT * FROM ad_per '.$filter->join().' WHERE '.$filter->where().' ORDER BY nachname, vorname;';
 	global $from_with_get;
 	global $treffer;
+
 	$erg = mysql_query($sql);
 	if (mysql_error() != "") {
 		echo '<br />'.$sql;
@@ -35,106 +43,82 @@ else {
 	printf(_('Searching for the term %s').' &hellip;', '<em>[ '.$suche.' ]</em>');
 	echo '<br /><br />';
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per WHERE CONCAT_WS(" ", vorname, nachname) like "%'.$suche.'%" || CONCAT_WS(" ", vorname, mittelname, nachname) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr WHERE fmg_lr='.$_SESSION['f'].' && (CONCAT_WS(" ", vorname, nachname) like "%'.$suche.'%" || CONCAT_WS(" ", vorname, mittelname, nachname) like "%'.$suche.'%") ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a name').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_where('(CONCAT_WS(" ", vorname, nachname) like "%'.$suche.'%" || CONCAT_WS(" ", vorname, mittelname, nachname) like "%'.$suche.'%")');
+	display_name_list($filter, '&hellip; '._('as part of a name').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_orte ON ort_r=o_id LEFT JOIN ad_plz ON plz_r=plz_id LEFT JOIN ad_laender ON land_r=l_id WHERE CONCAT_WS(", ", strasse, plz, ortsname, land) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_orte ON ort_r=o_id LEFT JOIN ad_plz ON plz_r=plz_id LEFT JOIN ad_laender ON land_r=l_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS(", ", strasse, plz, ortsname, land) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of an address').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_where('CONCAT_WS(", ", strasse, plz, ortsname, land) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of an address').':');
 
 
-	// telephone numbers associated with address
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_privat_r=v_id WHERE CONCAT_WS("-", vorwahl, ftel_privat) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_privat_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, ftel_privat) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a house phone number (private)').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON fvw_privat_r = v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, ftel_privat) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a house phone number (private)').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_arbeit_r=v_id WHERE CONCAT_WS("-", vorwahl, ftel_arbeit) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_arbeit_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, ftel_arbeit) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a house phone number (work)').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON fvw_arbeit_r=v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, ftel_arbeit) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a house phone number (work)').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_mobil_r=v_id WHERE CONCAT_WS("-", vorwahl, ftel_mobil) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_mobil_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, ftel_mobil) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a house phone number (mobile)').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON fvw_mobil_r=v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, ftel_mobil) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a house phone number (mobile)').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_fax_r=v_id WHERE CONCAT_WS("-", vorwahl, ftel_fax) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_fax_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, ftel_fax) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a house fax number').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON fvw_fax_r=v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, ftel_fax) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a house fax number').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_aux_r=v_id WHERE CONCAT_WS("-", vorwahl, ftel_aux) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_adressen ON adresse_r=ad_id LEFT JOIN ad_vorwahlen ON fvw_aux_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, ftel_aux) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a house phone number (aux)').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON fvw_aux_r=v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, ftel_aux) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a house phone number (aux)').':');
 
 
-	// telephone numbers associated with person
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_vorwahlen ON vw_privat_r=v_id WHERE CONCAT_WS("-", vorwahl, tel_privat) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_vorwahlen ON vw_privat_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, tel_privat) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a personal phone number (private)').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON vw_privat_r=v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, tel_privat) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a personal phone number (private)').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_vorwahlen ON vw_arbeit_r=v_id WHERE CONCAT_WS("-", vorwahl, tel_arbeit) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_vorwahlen ON vw_arbeit_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, tel_arbeit) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a personal phone number (work)').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON vw_arbeit_r=v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, tel_arbeit) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a personal phone number (work)').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_vorwahlen ON vw_mobil_r=v_id WHERE CONCAT_WS("-", vorwahl, tel_mobil) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_vorwahlen ON vw_mobil_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, tel_mobil) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a personal phone number (mobile)').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON vw_mobil_r=v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, tel_mobil) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a personal phone number (mobile)').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_vorwahlen ON vw_fax_r=v_id WHERE CONCAT_WS("-", vorwahl, tel_fax) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_vorwahlen ON vw_fax_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, tel_fax) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a personal fax number').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON vw_fax_r=v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, tel_fax) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a personal fax number').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_vorwahlen ON vw_aux_r=v_id WHERE CONCAT_WS("-", vorwahl, tel_aux) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr LEFT JOIN ad_vorwahlen ON vw_aux_r=v_id WHERE fmg_lr='.$_SESSION['f'].' && CONCAT_WS("-", vorwahl, tel_aux) like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a personal phone number (aux)').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_join('LEFT JOIN ad_vorwahlen ON vw_aux_r=v_id');
+	$filter->add_where('CONCAT_WS("-", vorwahl, tel_aux) like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a personal phone number (aux)').':');
 
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per WHERE email_privat like "%'.$suche.'%" || email_arbeit like "%'.$suche.'%" || email_aux like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr WHERE fmg_lr='.$_SESSION['f'].' && (email_privat like "%'.$suche.'%" || email_arbeit like "%'.$suche.'%" || email_aux like "%'.$suche.'%") ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of an email address').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_where('(email_privat like "%'.$suche.'%" || email_arbeit like "%'.$suche.'%" || email_aux like "%'.$suche.'%")');
+	display_name_list($filter, '&hellip; '._('as part of an email address').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per WHERE hp1 like "%'.$suche.'%" || hp2 like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr WHERE fmg_lr='.$_SESSION['f'].' && (hp1 like "%'.$suche.'%" || hp2 like "%'.$suche.'%") ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a URL').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_where('(hp1 like "%'.$suche.'%" || hp2 like "%'.$suche.'%")');
+	display_name_list($filter, '&hellip; '._('as part of a URL').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per WHERE chat_aim like "%'.$suche.'%" || chat_msn like "%'.$suche.'%" || chat_icq like "%'.$suche.'%" || chat_yim like "%'.$suche.'%" || chat_skype like "%'.$suche.'%" || chat_aux like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr WHERE fmg_lr='.$_SESSION['f'].' && (chat_aim like "%'.$suche.'%" || chat_msn like "%'.$suche.'%" || chat_icq like "%'.$suche.'%" || chat_yim like "%'.$suche.'%" || chat_skype like "%'.$suche.'%" || chat_aux like "%'.$suche.'%") ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a chat nickname').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_where('(chat_aim like "%'.$suche.'%" || chat_msn like "%'.$suche.'%" || chat_icq like "%'.$suche.'%" || chat_yim like "%'.$suche.'%" || chat_skype like "%'.$suche.'%" || chat_aux like "%'.$suche.'%")');
+	display_name_list($filter, '&hellip; '._('as part of a chat nickname').':');
 
-	if ($_SESSION['f'] == 0)
-		$sql = 'SELECT * FROM ad_per WHERE pnotizen like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	else
-		$sql = 'SELECT * FROM ad_per LEFT JOIN ad_flinks ON p_id=person_lr WHERE fmg_lr='.$_SESSION['f'].' && pnotizen like "%'.$suche.'%" ORDER BY nachname, vorname;';
-	display_name_list($sql, '&hellip; '._('as part of a note').':');
+	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+	$filter->add_where('pnotizen like "%'.$suche.'%"');
+	display_name_list($filter, '&hellip; '._('as part of a note').':');
 
 	if (!$treffer) {
 		echo _('no results').'<br /><br />';
