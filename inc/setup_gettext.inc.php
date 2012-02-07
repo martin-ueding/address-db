9a60b2c8c99250d29b35e04ac6916e7dbe29b298
Martin Ueding <dev@martin-ueding.de>
2012-02-07 13:20:47 +0100

    Store domain in a var

--- inc/setup_gettext.inc.php
+++ inc/setup_gettext.inc.php
@@ -15,12 +15,15 @@ else if (empty($_SESSION['lang'])) {
 else {
 	$lang = $_SESSION['lang'];
 }
+
 if (function_exists('bindtextdomain')) {
+	$domain = 'address_db';
+
 	putenv('LC_MESSAGES='.$lang);
 	setlocale(LC_MESSAGES, $lang);
-	bindtextdomain("phpfamilyaddressdb", "../locale/");
-	bind_textdomain_codeset("phpfamilyaddressdb", "iso-8859-1");
-	textdomain("phpfamilyaddressdb");
+	bindtextdomain($domain, "../locale/");
+	bind_textdomain_codeset($domain, "iso-8859-1");
+	textdomain($domain);
 
 	$available_languages = array(
 		array('de_DE', _('German')),
