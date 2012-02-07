2b8b2711a6abcce6274fd4718471fa14688c2f8b
Martin Ueding <dev@martin-ueding.de>
2012-02-07 15:23:57 +0100

    Single quotes

--- adressen/inc/includes.inc.php
+++ adressen/inc/includes.inc.php
@@ -3,7 +3,7 @@
 
 include('_config.inc.php');
 include('inc/abfragen.inc.php');
-include("inc/anzeigen.inc.php");
-include("inc/select.inc.php");
-include("inc/varclean.inc.php");
+include('inc/anzeigen.inc.php');
+include('inc/select.inc.php');
+include('inc/varclean.inc.php');
 ?>
