80293750693e65e2b27391789aec3294d2821e50
Martin Ueding <dev@martin-ueding.de>
2012-02-07 18:34:59 +0100

    Remove newlines

--- adressen/pages/pic_upload2.inc.php
+++ adressen/pages/pic_upload2.inc.php
@@ -7,23 +7,23 @@ if (file_exists($dateiname)) {
 
 	$bilddaten = getimagesize($dateiname);
 	
-	echo '<applet code="BildAusschnitt" width="'.($bilddaten[0]).'" height="'.($bilddaten[1]+30).'">'."\n";
-	echo '<param name="bildurl" value="'.$dateiname.'"></param>'."\n";
-	echo '<param name="archive" value="ba.jar"></param>'."\n";
-	echo '<param name="rot" value="255"></param>'."\n";
-	echo '<param name="gruen" value="255"></param>'."\n";
-	echo '<param name="blau" value="0"></param>'."\n";
-	echo '<param name="rot2" value="0"></param>'."\n";
-	echo '<param name="gruen2" value="255"></param>'."\n";
-	echo '<param name="blau2" value="255"></param>'."\n";
-	echo '<param name="anfasserbreite" value="5"></param>'."\n";
-	echo '<param name="seitenv" value="0"></param>'."\n";
-	echo '<param name="zieladresse" value="'.$url_to_server.'adressen/index.php?mode=pic_upload3"></param>'."\n";
-	echo '</applet>'."\n";
+	echo '<applet code="BildAusschnitt" width="'.($bilddaten[0]).'" height="'.($bilddaten[1]+30).'">';
+	echo '<param name="bildurl" value="'.$dateiname.'"></param>';
+	echo '<param name="archive" value="ba.jar"></param>';
+	echo '<param name="rot" value="255"></param>';
+	echo '<param name="gruen" value="255"></param>';
+	echo '<param name="blau" value="0"></param>';
+	echo '<param name="rot2" value="0"></param>';
+	echo '<param name="gruen2" value="255"></param>';
+	echo '<param name="blau2" value="255"></param>';
+	echo '<param name="anfasserbreite" value="5"></param>';
+	echo '<param name="seitenv" value="0"></param>';
+	echo '<param name="zieladresse" value="'.$url_to_server.'adressen/index.php?mode=pic_upload3"></param>';
+	echo '</applet>';
 
 }
 else {
-	printf(_('There is no file %s.');, $dateiname);
+	printf(_('There is no file %s.'), $dateiname);
 }
 
 ?>
