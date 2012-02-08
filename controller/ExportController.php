6ed539671058cd73b449d5b40382e409bcbbfba4
Martin Ueding <dev@martin-ueding.de>
2012-02-08 18:39:51 +0100

    Add kitchen export

--- controller/ExportController.php
+++ controller/ExportController.php
@@ -232,5 +232,124 @@ class ExportController extends Controller {
 
 		die();
 	}
+
+	public function kitchen() {
+		$SCHRIFTGROESSE = 9;
+		header("Content-Type: text/plain; charset=iso-8859-1");
+		header('Content-Disposition: attachment; filename="adressen-'.time().'.tex"');
+
+		function convertToLaTeX ($s) {
+			$s = str_replace('@', '(at)', $s);
+			$s = str_replace('_', '\\_', $s);
+			return $s;
+		}
+
+
+		$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+		$filter->add_join('LEFT JOIN ad_adressen ON adresse_r = ad_id');
+		$filter->add_join('LEFT JOIN ad_anreden ON anrede_r = a_id');
+		$filter->add_join('LEFT JOIN ad_laender ON land_r = l_id');
+		$filter->add_join('LEFT JOIN ad_orte ON ort_r = o_id');
+		$filter->add_join('LEFT JOIN ad_plz ON plz_r = plz_id');
+		$filter->add_join('LEFT JOIN ad_prafixe ON prafix_r = prafix_id');
+		$filter->add_join('LEFT JOIN ad_suffixe ON suffix_r = s_id');
+
+		$sql = 'SELECT * FROM ad_per '.$filter->join().' WHERE '.$filter->where().' ORDER BY nachname, vorname;';
+
+		echo '\documentclass[10pt]{article}
+		\usepackage[left=7mm, right=7mm, top=7mm, bottom=7mm, scale=1, landscape]{geometry}
+		\geometry{a4paper}
+		\setlength{\parindent}{0cm}
+		\usepackage[latin1]{inputenc}
+		\usepackage{multicol}
+		\renewcommand\sfdefault{phv}
+		\renewcommand\familydefault{\sfdefault}
+		';
+
+
+		echo '\begin{document}'."\n";
+
+		//echo '\fontsize{'.$SCHRIFTGROESSE.'}{'.round($SCHRIFTGROESSE*1.4).'}'."\n";
+		//echo '\selectfont'."\n";
+
+
+		function bruch () {
+			return "\n\n\\nopagebreak[4]";
+		}
+		echo '\begin{multicols}{4}';
+
+
+		$erg = mysql_query($sql);
+		while ($l = mysql_fetch_assoc($erg)) {
+
+			if ($l['prafix'] != "-")
+				$prafix = $l['prafix'];
+			else
+				$prafix = '';
+
+			$display_name = trim($prafix.' '.$l['nachname'].', '.$l['vorname'], ' ,');
+			$name = '\section*{'.$display_name.'}'."\n";
+
+			$content = '';
+
+			if (isset($last) && $last['adresse_r'] != $l['adresse_r']) {
+				if ($l['adresse_r'] != 1) {
+					$content .= $l['strasse'].bruch().$l['ortsname'].' '.$l['plz'].bruch();
+				}
+			}
+
+			// TODO i18n
+			if (!empty($l['tel_privat']))
+				$content .= 'Tel Privat: '.AreaCode::select_vw_id($l['vw_privat_r']).'-'.$l['tel_privat'].bruch();
+
+			if (!empty($l['tel_arbeit']))
+				$content .= 'Tel Arbeit: '.AreaCode::select_vw_id($l['vw_arbeit_r']).'-'.$l['tel_arbeit'].bruch();
+
+			if (!empty($l['tel_mobil']))
+				$content .= 'Handy: '.AreaCode::select_vw_id($l['vw_mobil_r']).'-'.$l['tel_mobil'].bruch();
+
+			if (!empty($l['tel_fax']))
+				$content .= 'Fax: '.AreaCode::select_vw_id($l['vw_fax_r']).'-'.$l['tel_fax'].bruch();
+
+			if (!empty($l['tel_aux']))
+				$content .= 'Tel: '.AreaCode::select_vw_id($l['vw_aux_r']).'-'.$l['tel_aux'].bruch();
+
+			if (isset($last) && $last['adresse_r'] != $l['adresse_r']) {
+				if (!empty($l['ftel_privat']))
+					$content .= 'Tel Privat: '.AreaCode::select_vw_id($l['fvw_privat_r']).'-'.$l['ftel_privat'].bruch();
+
+				if (!empty($l['ftel_arbeit']))
+					$content .= 'Tel Arbeit: '.AreaCode::select_vw_id($l['fvw_arbeit_r']).'-'.$l['ftel_arbeit'].bruch();
+
+				if (!empty($l['ftel_mobil']))
+					$content .= 'Handy: '.AreaCode::select_vw_id($l['fvw_mobil_r']).'-'.$l['ftel_mobil'].bruch();
+
+				if (!empty($l['ftel_fax']))
+					$content .= 'Fax: '.AreaCode::select_vw_id($l['fvw_fax_r']).'-'.$l['ftel_fax'].bruch();
+
+				if (!empty($l['ftel_aux']))
+					$content .= 'Tel: '.AreaCode::select_vw_id($l['fvw_aux_r']).'-'.$l['ftel_aux'].bruch();
+			}
+
+
+			if (strlen($content) > 0) {
+				echo $name;
+				echo $content;
+
+				if (!empty($l['pnotizen'])) {
+					echo '\\begin{verbatim}'.wordwrap($l['pnotizen'], 33, "\n", true).'\\end{verbatim}'.bruch();
+				}
+
+			}
+
+			$last = $l;
+		}
+
+		echo '\end{multicols}';
+
+		echo '\end{document}';
+
+		die();
+	}
 }
 ?>
