814633b6f26566919c942ff9b9966bd017c9f606
Martin Ueding <dev@martin-ueding.de>
2012-02-08 19:13:02 +0100

    Highlight associated names

--- template/person_delete.php
+++ template/person_delete.php
@@ -2,16 +2,21 @@
 <?php
 printf(_('Do you really want to delete the entry %s?'), '<em>'.$person_loop['vorname'].' '.$person_loop['nachname'].'</em>');
 
+$highlighter = function($string) {return '<em>'.$string.'</em>';};
+
 if (isset($association_ids)) {
 	if (count($association_ids) >= 2) {
 		echo '<br />';
 		echo '<br />';
-		echo _('Warning:').' '.sprintf(_('This entry is connected to %s. Consider just removing your association with it.'), implode(', ', $association_names));
+		echo _('Warning:').' '.sprintf(
+			_('This entry is connected to %s. Consider just removing your association with it.'),
+			implode(', ', array_map($highlighter, $association_names))
+		);
 	}
 	else if (count($association_ids) == 1 && $association_ids[0] != $_SESSION['f']) {
 		echo '<br />';
 		echo '<br />';
-		echo _('Warning:').' '.sprintf(_('This entry is connected to %s. Are you sure that you can delete this entry?'), $association_names[0]);
+		echo _('Warning:').' '.sprintf(_('This entry is connected to %s. Are you sure that you can delete this entry?'), '<em>'.$association_names[0].'</em>');
 	}
 }
 
