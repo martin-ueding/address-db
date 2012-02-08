eda61a5df5a7d943f393550c7be54bb01b1f429f
Martin Ueding <dev@martin-ueding.de>
2012-02-08 12:57:24 +0100

    Removed includes file

--- inc/includes.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-# Copyright © 2011 Martin Ueding <dev@martin-ueding.de>
-
-include('_config.inc.php');
-include('inc/abfragen.inc.php');
-include('inc/anzeigen.inc.php');
-include('inc/select.inc.php');
-include('inc/varclean.inc.php');
-?>
