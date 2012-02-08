8b8608ffd010821a7e91dca2db8ce8a66e004961
Martin Ueding <dev@martin-ueding.de>
2012-02-08 12:28:12 +0100

    Slide out messages on demand

--- adressen/js/message_box.js
+++ adressen/js/message_box.js
@@ -2,5 +2,7 @@
 
 // Fadeout messages box.
 $(document).ready(function() {
-	$("#messages").fadeOut(0).fadeIn(500).delay('.(5000*count($msgs)).').slideUp(1000);
+	$("#messages").fadeOut(0).fadeIn(500).click(function () {
+		$(this).slideUp(1000);
+	})
 });
