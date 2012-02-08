f1372cf5ad33aff4d965a4738c066a872791c26b
Martin Ueding <dev@martin-ueding.de>
2012-02-08 16:09:41 +0100

    Code formatting

--- controller/ListController.php
+++ controller/ListController.php
@@ -2,103 +2,102 @@
 # Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
 
 require_once('component/Filter.php');
+require_once('controller/Controller.php');
 require_once('helper/Table.php');
 require_once('model/FamilyMember.php');
 require_once('model/Group.php');
-require_once('controller/Controller.php');
 
 class ListController extends Controller {
 	public function index() {
-	echo '<h1>'._('list').'</h1>';
-	$from_with_get = 'mode=list';
+		$from_with_get = 'mode=list';
 
-	/* Daten sammeln */
-	$filter = new Filter($_SESSION['f'], $_SESSION['g']);
+		/* Daten sammeln */
+		$filter = new Filter($_SESSION['f'], $_SESSION['g']);
 
-	/* Suche nach Buchstabe */
-	if (!empty($_GET['b'])) {
-		$from_with_get .= '&b='.$_GET['b'];
-		$filter->add_where('nachname like "'.$_GET['b'].'%"');
-	}
-	/* Suche nach Gruppe */
-	else if (!empty($_GET['g'])) {
-		$from_with_get .= '&g='.$_GET['g'];
-	}
-	/* Suche nach Bezug */
-	else if (!empty($_GET['f'])) {
-		$from_with_get .= '&f='.$_GET['f'];
-	}
-
-	$sql = 'SELECT * FROM ad_per '.$filter->join().' WHERE '.$filter->where().' ORDER BY nachname, vorname;';
-
-	/* Daten anzeigen */
-	if (!empty($sql)) {
-		$erg = mysql_query($sql);
-
+		/* Suche nach Buchstabe */
 		if (!empty($_GET['b'])) {
-			printf(
-				_('Last names starting with %s:'),
-				'<em>'.$_GET['b'].'</em>', mysql_num_rows($erg)
-			);
-			echo '<br />';
+			$from_with_get .= '&b='.$_GET['b'];
+			$filter->add_where('nachname like "'.$_GET['b'].'%"');
+		}
+		/* Suche nach Gruppe */
+		else if (!empty($_GET['g'])) {
+			$from_with_get .= '&g='.$_GET['g'];
+		}
+		/* Suche nach Bezug */
+		else if (!empty($_GET['f'])) {
+			$from_with_get .= '&f='.$_GET['f'];
 		}
 
-		else if ($_SESSION['g'] != 0) {
-			printf(
-				_('Group %s:'),
-				'<em>'.Group::get_name($_SESSION['g']).'</em>', mysql_num_rows($erg)
-			);
-			echo '<br />';
-		}
+		$sql = 'SELECT * FROM ad_per '.$filter->join().' WHERE '.$filter->where().' ORDER BY nachname, vorname;';
 
-		else if ($_SESSION['f'] != 0) {
-			printf(
-				_('Member %s:'),
-				'<em>'.FamilyMember::get_name($_SESSION['f']).'</em>', mysql_num_rows($erg)
-			);
-			echo '<br />';
-		}
+		/* Daten anzeigen */
+		if (!empty($sql)) {
+			$erg = mysql_query($sql);
 
-		printf(
-			ngettext(
-				'%d entry:',
-				'%d entries:', mysql_num_rows($erg)
-			),
-			mysql_num_rows($erg)
-		);
-
-		echo '<br /><br />';
-
-		$table = new Table($erg, $from_with_get);
-		echo $table->html();
-
-		// Collect email address from everybody to send off a mass email.
-		$erg = mysql_query($sql);
-		while ($l = mysql_fetch_assoc($erg)) {
-			if ($l['email_privat'] != "") {
-				$emailadressen[] = $l['email_privat'];
+			if (!empty($_GET['b'])) {
+				printf(
+					_('Last names starting with %s:'),
+					'<em>'.$_GET['b'].'</em>', mysql_num_rows($erg)
+				);
+				echo '<br />';
 			}
-			else if ($l['email_arbeit'] != "") {
-				$emailadressen[] = $l['email_arbeit'];
+
+			else if ($_SESSION['g'] != 0) {
+				printf(
+					_('Group %s:'),
+					'<em>'.Group::get_name($_SESSION['g']).'</em>', mysql_num_rows($erg)
+				);
+				echo '<br />';
 			}
-			else if ($l['email_aux'] != "") {
-				$emailadressen[] = $l['email_aux'];
+
+			else if ($_SESSION['f'] != 0) {
+				printf(
+					_('Member %s:'),
+					'<em>'.FamilyMember::get_name($_SESSION['f']).'</em>', mysql_num_rows($erg)
+				);
+				echo '<br />';
+			}
+
+			printf(
+				ngettext(
+					'%d entry:',
+					'%d entries:', mysql_num_rows($erg)
+				),
+				mysql_num_rows($erg)
+			);
+
+			echo '<br /><br />';
+
+			$table = new Table($erg, $from_with_get);
+			echo $table->html();
+
+			// Collect email address from everybody to send off a mass email.
+			$erg = mysql_query($sql);
+			while ($l = mysql_fetch_assoc($erg)) {
+				if ($l['email_privat'] != "") {
+					$emailadressen[] = $l['email_privat'];
+				}
+				else if ($l['email_arbeit'] != "") {
+					$emailadressen[] = $l['email_arbeit'];
+				}
+				else if ($l['email_aux'] != "") {
+					$emailadressen[] = $l['email_aux'];
+				}
 			}
 		}
-	}
 
-	if (empty($_GET['b']) && empty($_GET['g']) && !empty($_GET['f'])) {
-		echo '<br /><br />';
-		echo '<a href="export/vcard_fmg.php?f='.$_SESSION['f'].'">'._('export this list as a VCard').'</a>';
-		echo '<br />';
-		echo '<a href="export/export812_fmg.php?f='.$_SESSION['f'].'">'._('export this list as a LaTeX for day planner sheets').'</a>';
+		if (empty($_GET['b']) && empty($_GET['g']) && !empty($_GET['f'])) {
+			echo '<br /><br />';
+			echo '<a href="export/vcard_fmg.php?f='.$_SESSION['f'].'">'._('export this list as a VCard').'</a>';
+			echo '<br />';
+			echo '<a href="export/export812_fmg.php?f='.$_SESSION['f'].'">'._('export this list as a LaTeX for day planner sheets').'</a>';
 
-	}
+		}
 
-	if (!empty($emailadressen)) {
-		echo '<br /><br />';
-		echo '<a href="mailto:?bcc='.implode(',', $emailadressen).'">'._('send an email to everybody in this list').'</a>';
-	}
+		if (!empty($emailadressen)) {
+			echo '<br /><br />';
+			echo '<a href="mailto:?bcc='.implode(',', $emailadressen).'">'._('send an email to everybody in this list').'</a>';
+		}
 	}
 }
 ?>
