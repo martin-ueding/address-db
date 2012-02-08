46bde6c30b3c1d32b576b92dade10c9cc66dd94a
Martin Ueding <dev@martin-ueding.de>
2012-02-08 19:21:04 +0100

    Wrap code up into functions

--- inc/setup_gettext.inc.php
+++ inc/setup_gettext.inc.php
@@ -1,41 +1,49 @@
 <?php
 # Copyright © 2011 Martin Ueding <dev@martin-ueding.de>
 
-if (session_id() == "")
-	session_start();
+function gtxt_find_language() {
+	if (session_id() == "")
+		session_start();
 
-if (!empty($_GET['lang'])) {
-	$_SESSION['lang'] = $_GET['lang'];
-	$lang = $_GET['lang'];
-}
-else if (empty($_SESSION['lang'])) {
-	$_SESSION['lang'] = 'de_DE';
-	$lang = 'de_DE';
-}
-else {
-	$lang = $_SESSION['lang'];
+	if (!empty($_GET['lang'])) {
+		$_SESSION['lang'] = $_GET['lang'];
+		$lang = $_GET['lang'];
+	}
+	else if (empty($_SESSION['lang'])) {
+		$_SESSION['lang'] = 'de_DE';
+		$lang = 'de_DE';
+	}
+	else {
+		$lang = $_SESSION['lang'];
+	}
+
+	return $lang;
 }
 
-if (function_exists('bindtextdomain')) {
-	$domain = 'address_db';
+function gtxt_set_locale($lang) {
+	if (function_exists('bindtextdomain')) {
+		$domain = 'address_db';
 
-	putenv('LC_MESSAGES='.$lang);
-	setlocale(LC_MESSAGES, $lang);
-	bindtextdomain($domain, "../locale/");
-	bind_textdomain_codeset($domain, "iso-8859-1");
-	textdomain($domain);
+		putenv('LC_MESSAGES='.$lang);
+		setlocale(LC_MESSAGES, $lang);
+		bindtextdomain($domain, "../locale/");
+		bind_textdomain_codeset($domain, "iso-8859-1");
+		textdomain($domain);
 
-	$available_languages = array(
-		array('de_DE', _('German')),
-		array('en', _('English')),
-		array('nl', _('Dutch')),
-		array('tr', _('Turkish'))
-	);
-}
-else {
-	function _($s) {
-		return $s;
+		$available_languages = array(
+			array('de_DE', _('German')),
+			array('en', _('English')),
+			array('nl', _('Dutch')),
+			array('tr', _('Turkish'))
+		);
+	}
+	else {
+		function _($s) {
+			return $s;
+		}
 	}
 }
+
+gtxt_set_locale(gtxt_find_language());
 
 ?>
