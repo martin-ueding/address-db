a03a023c67774cf58c9104ad56d226618d39b950
Martin Ueding <dev@martin-ueding.de>
2012-02-10 17:46:10 +0100

    Update copyright strings

--- inc/setup_gettext.inc.php
+++ inc/setup_gettext.inc.php
@@ -1,5 +1,5 @@
 <?php
-# Copyright © 2011 Martin Ueding <dev@martin-ueding.de>
+# Copyright © 2011-2012 Martin Ueding <dev@martin-ueding.de>
 
 function gtxt_find_language() {
 	if (session_id() == "")
