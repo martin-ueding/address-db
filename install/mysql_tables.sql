fcfe3b2a0fa8483cfb55620838e08292a9174e6d
Martin Ueding <dev@martin-ueding.de>
2012-02-12 15:13:53 +0100

    Dump current database

--- install/mysql_tables.sql
+++ install/mysql_tables.sql
@@ -1,228 +1,136 @@
--- phpMyAdmin SQL Dump
--- version 2.6.4-pl3
--- http://www.phpmyadmin.net
--- 
--- Host: rdbms.strato.de
--- Erstellungszeit: 18. Oktober 2010 um 18:00
--- Server Version: 5.0.89
--- PHP-Version: 5.2.9
+CREATE TABLE `ad_adressen` (
+  `ad_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `ftel_privat` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `ftel_arbeit` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `ftel_mobil` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `ftel_fax` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `ftel_aux` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `fvw_privat_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `fvw_arbeit_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `fvw_mobil_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `fvw_fax_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `fvw_aux_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `strasse` varchar(200) COLLATE latin1_german1_ci DEFAULT NULL,
+  `plz_r` mediumint(8) unsigned NOT NULL DEFAULT '1',
+  `ort_r` mediumint(8) unsigned NOT NULL DEFAULT '1',
+  `land_r` mediumint(8) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`ad_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=233 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `ad_anreden` (
+  `a_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `anrede` varchar(30) COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- 
--- Tabellenstruktur für Tabelle `ad_adressen`
--- 
-
-DROP TABLE IF EXISTS `ad_adressen`;
-CREATE TABLE IF NOT EXISTS `ad_adressen` (
-  `ad_id` int(5) unsigned NOT NULL auto_increment,
-  `ftel_privat` varchar(50) collate latin1_german1_ci default NULL,
-  `ftel_arbeit` varchar(50) collate latin1_german1_ci default NULL,
-  `ftel_mobil` varchar(50) collate latin1_german1_ci default NULL,
-  `ftel_fax` varchar(50) collate latin1_german1_ci default NULL,
-  `ftel_aux` varchar(50) collate latin1_german1_ci default NULL,
-  `fvw_privat_r` int(5) unsigned NOT NULL default '1',
-  `fvw_arbeit_r` int(5) unsigned NOT NULL default '1',
-  `fvw_mobil_r` int(5) unsigned NOT NULL default '1',
-  `fvw_fax_r` int(5) unsigned NOT NULL default '1',
-  `fvw_aux_r` int(5) unsigned NOT NULL default '1',
-  `strasse` varchar(200) collate latin1_german1_ci default NULL,
-  `plz_r` mediumint(8) unsigned NOT NULL default '1',
-  `ort_r` mediumint(8) unsigned NOT NULL default '1',
-  `land_r` mediumint(8) unsigned NOT NULL default '1',
-  PRIMARY KEY  (`ad_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=210 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=210 ;
-
--- --------------------------------------------------------
-
--- 
--- Tabellenstruktur für Tabelle `ad_anreden`
--- 
-
-DROP TABLE IF EXISTS `ad_anreden`;
-CREATE TABLE IF NOT EXISTS `ad_anreden` (
-  `a_id` int(10) unsigned NOT NULL auto_increment,
-  `anrede` varchar(30) collate latin1_german1_ci NOT NULL,
-  PRIMARY KEY  (`a_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=7 ;
-
--- --------------------------------------------------------
-
--- 
--- Tabellenstruktur für Tabelle `ad_flinks`
--- 
-
-DROP TABLE IF EXISTS `ad_flinks`;
-CREATE TABLE IF NOT EXISTS `ad_flinks` (
-  `fl_id` int(5) unsigned NOT NULL auto_increment,
+CREATE TABLE `ad_flinks` (
+  `fl_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
   `fmg_lr` int(5) unsigned NOT NULL,
   `person_lr` int(5) unsigned NOT NULL,
-  PRIMARY KEY  (`fl_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1747 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=1747 ;
+  PRIMARY KEY (`fl_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2015 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `ad_fmg` (
+  `fmg_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `fmg` varchar(50) COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`fmg_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- 
--- Tabellenstruktur für Tabelle `ad_fmg`
--- 
-
-DROP TABLE IF EXISTS `ad_fmg`;
-CREATE TABLE IF NOT EXISTS `ad_fmg` (
-  `fmg_id` int(5) unsigned NOT NULL auto_increment,
-  `fmg` varchar(50) collate latin1_german1_ci NOT NULL,
-  PRIMARY KEY  (`fmg_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=5 ;
-
--- --------------------------------------------------------
-
--- 
--- Tabellenstruktur für Tabelle `ad_glinks`
--- 
-
-DROP TABLE IF EXISTS `ad_glinks`;
-CREATE TABLE IF NOT EXISTS `ad_glinks` (
-  `gl_id` int(5) unsigned NOT NULL auto_increment,
+CREATE TABLE `ad_glinks` (
+  `gl_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
   `person_lr` int(5) unsigned NOT NULL,
   `gruppe_lr` int(5) unsigned NOT NULL,
-  PRIMARY KEY  (`gl_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1126 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=1126 ;
+  PRIMARY KEY (`gl_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1246 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `ad_gruppen` (
+  `g_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `gruppe` varchar(100) COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`g_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- 
--- Tabellenstruktur für Tabelle `ad_gruppen`
--- 
+CREATE TABLE `ad_hu` (
+  `hu_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `hu_kennzeichen` varchar(15) COLLATE latin1_german1_ci NOT NULL DEFAULT 'BN-XX XXXX',
+  `hu_name` varchar(100) COLLATE latin1_german1_ci NOT NULL,
+  `hu_monat` int(2) unsigned NOT NULL DEFAULT '1',
+  `hu_jahr` int(4) unsigned NOT NULL DEFAULT '2000',
+  PRIMARY KEY (`hu_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
-DROP TABLE IF EXISTS `ad_gruppen`;
-CREATE TABLE IF NOT EXISTS `ad_gruppen` (
-  `g_id` int(5) unsigned NOT NULL auto_increment,
-  `gruppe` varchar(100) collate latin1_german1_ci NOT NULL,
-  PRIMARY KEY  (`g_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=20 ;
+CREATE TABLE `ad_laender` (
+  `l_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `land` varchar(100) COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`l_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `ad_orte` (
+  `o_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ortsname` varchar(100) COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`o_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=74 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- 
--- Tabellenstruktur für Tabelle `ad_laender`
--- 
+CREATE TABLE `ad_per` (
+  `p_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `vorname` varchar(100) COLLATE latin1_german1_ci DEFAULT NULL,
+  `mittelname` varchar(100) COLLATE latin1_german1_ci DEFAULT NULL,
+  `nachname` varchar(100) COLLATE latin1_german1_ci DEFAULT NULL,
+  `anrede_r` smallint(5) unsigned NOT NULL DEFAULT '1',
+  `prafix_r` smallint(5) unsigned NOT NULL DEFAULT '1',
+  `suffix_r` smallint(5) unsigned NOT NULL DEFAULT '1',
+  `tel_privat` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `tel_arbeit` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `tel_mobil` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `tel_fax` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `tel_aux` varchar(50) COLLATE latin1_german1_ci DEFAULT NULL,
+  `vw_privat_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `vw_arbeit_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `vw_mobil_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `vw_fax_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `vw_aux_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `email_privat` varchar(150) COLLATE latin1_german1_ci DEFAULT NULL,
+  `email_arbeit` varchar(150) COLLATE latin1_german1_ci DEFAULT NULL,
+  `email_aux` varchar(150) COLLATE latin1_german1_ci DEFAULT NULL,
+  `hp1` varchar(250) COLLATE latin1_german1_ci DEFAULT NULL,
+  `hp2` varchar(250) COLLATE latin1_german1_ci DEFAULT NULL,
+  `geb_t` smallint(2) unsigned DEFAULT NULL,
+  `geb_m` smallint(2) unsigned DEFAULT NULL,
+  `geb_j` int(4) unsigned DEFAULT NULL,
+  `chat_aim` varchar(150) COLLATE latin1_german1_ci DEFAULT NULL,
+  `chat_msn` varchar(150) COLLATE latin1_german1_ci DEFAULT NULL,
+  `chat_icq` varchar(9) COLLATE latin1_german1_ci DEFAULT NULL,
+  `chat_yim` varchar(150) COLLATE latin1_german1_ci DEFAULT NULL,
+  `chat_skype` varchar(150) COLLATE latin1_german1_ci DEFAULT NULL,
+  `chat_aux` varchar(150) COLLATE latin1_german1_ci DEFAULT NULL,
+  `geburtsname` varchar(100) COLLATE latin1_german1_ci DEFAULT NULL,
+  `pnotizen` text COLLATE latin1_german1_ci,
+  `adresse_r` int(5) unsigned NOT NULL DEFAULT '1',
+  `last_check` int(16) unsigned NOT NULL DEFAULT '0',
+  `last_send` int(16) NOT NULL DEFAULT '0',
+  `last_edit` int(16) NOT NULL,
+  PRIMARY KEY (`p_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=583 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
-DROP TABLE IF EXISTS `ad_laender`;
-CREATE TABLE IF NOT EXISTS `ad_laender` (
-  `l_id` int(5) unsigned NOT NULL auto_increment,
-  `land` varchar(100) collate latin1_german1_ci NOT NULL,
-  PRIMARY KEY  (`l_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=10 ;
+CREATE TABLE `ad_plz` (
+  `plz_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `plz` varchar(15) COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`plz_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=86 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `ad_prafixe` (
+  `prafix_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `prafix` varchar(30) COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`prafix_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
--- 
--- Tabellenstruktur für Tabelle `ad_orte`
--- 
+CREATE TABLE `ad_suffixe` (
+  `s_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `suffix` varchar(30) COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`s_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
-DROP TABLE IF EXISTS `ad_orte`;
-CREATE TABLE IF NOT EXISTS `ad_orte` (
-  `o_id` int(10) unsigned NOT NULL auto_increment,
-  `ortsname` varchar(100) collate latin1_german1_ci NOT NULL,
-  PRIMARY KEY  (`o_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=69 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=69 ;
-
--- --------------------------------------------------------
-
--- 
--- Tabellenstruktur für Tabelle `ad_per`
--- 
-
-DROP TABLE IF EXISTS `ad_per`;
-CREATE TABLE IF NOT EXISTS `ad_per` (
-  `p_id` int(5) unsigned NOT NULL auto_increment,
-  `vorname` varchar(100) collate latin1_german1_ci default NULL,
-  `mittelname` varchar(100) collate latin1_german1_ci default NULL,
-  `nachname` varchar(100) collate latin1_german1_ci default NULL,
-  `anrede_r` smallint(5) unsigned NOT NULL default '1',
-  `prafix_r` smallint(5) unsigned NOT NULL default '1',
-  `suffix_r` smallint(5) unsigned NOT NULL default '1',
-  `tel_privat` varchar(50) collate latin1_german1_ci default NULL,
-  `tel_arbeit` varchar(50) collate latin1_german1_ci default NULL,
-  `tel_mobil` varchar(50) collate latin1_german1_ci default NULL,
-  `tel_fax` varchar(50) collate latin1_german1_ci default NULL,
-  `tel_aux` varchar(50) collate latin1_german1_ci default NULL,
-  `vw_privat_r` int(5) unsigned NOT NULL default '1',
-  `vw_arbeit_r` int(5) unsigned NOT NULL default '1',
-  `vw_mobil_r` int(5) unsigned NOT NULL default '1',
-  `vw_fax_r` int(5) unsigned NOT NULL default '1',
-  `vw_aux_r` int(5) unsigned NOT NULL default '1',
-  `email_privat` varchar(150) collate latin1_german1_ci default NULL,
-  `email_arbeit` varchar(150) collate latin1_german1_ci default NULL,
-  `email_aux` varchar(150) collate latin1_german1_ci default NULL,
-  `hp1` varchar(250) collate latin1_german1_ci default NULL,
-  `hp2` varchar(250) collate latin1_german1_ci default NULL,
-  `geb_t` smallint(2) unsigned default NULL,
-  `geb_m` smallint(2) unsigned default NULL,
-  `geb_j` int(4) unsigned default NULL,
-  `chat_aim` varchar(150) collate latin1_german1_ci default NULL,
-  `chat_msn` varchar(150) collate latin1_german1_ci default NULL,
-  `chat_icq` varchar(9) collate latin1_german1_ci default NULL,
-  `chat_yim` varchar(150) collate latin1_german1_ci default NULL,
-  `chat_skype` varchar(150) collate latin1_german1_ci default NULL,
-  `chat_aux` varchar(150) collate latin1_german1_ci default NULL,
-  `geburtsname` varchar(100) collate latin1_german1_ci default NULL,
-  `pnotizen` text collate latin1_german1_ci,
-  `adresse_r` int(5) unsigned NOT NULL default '1',
-  `last_check` int(16) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`p_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=528 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=528 ;
-
--- --------------------------------------------------------
-
--- 
--- Tabellenstruktur für Tabelle `ad_plz`
--- 
-
-DROP TABLE IF EXISTS `ad_plz`;
-CREATE TABLE IF NOT EXISTS `ad_plz` (
-  `plz_id` int(5) unsigned NOT NULL auto_increment,
-  `plz` varchar(15) collate latin1_german1_ci NOT NULL,
-  PRIMARY KEY  (`plz_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=76 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=76 ;
-
--- --------------------------------------------------------
-
--- 
--- Tabellenstruktur für Tabelle `ad_prafixe`
--- 
-
-DROP TABLE IF EXISTS `ad_prafixe`;
-CREATE TABLE IF NOT EXISTS `ad_prafixe` (
-  `prafix_id` int(10) unsigned NOT NULL auto_increment,
-  `prafix` varchar(30) collate latin1_german1_ci NOT NULL,
-  PRIMARY KEY  (`prafix_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=8 ;
-
--- --------------------------------------------------------
-
--- 
--- Tabellenstruktur für Tabelle `ad_suffixe`
--- 
-
-DROP TABLE IF EXISTS `ad_suffixe`;
-CREATE TABLE IF NOT EXISTS `ad_suffixe` (
-  `s_id` int(5) unsigned NOT NULL auto_increment,
-  `suffix` varchar(30) collate latin1_german1_ci NOT NULL,
-  PRIMARY KEY  (`s_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=6 ;
-
--- --------------------------------------------------------
-
--- 
--- Tabellenstruktur für Tabelle `ad_vorwahlen`
--- 
-
-DROP TABLE IF EXISTS `ad_vorwahlen`;
-CREATE TABLE IF NOT EXISTS `ad_vorwahlen` (
-  `v_id` int(5) unsigned NOT NULL auto_increment,
-  `vorwahl` varchar(20) collate latin1_german1_ci NOT NULL,
-  PRIMARY KEY  (`v_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=90 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=90 ;
-
-ALTER TABLE `ad_per` ADD `last_send` INT( 16 ) DEFAULT '0' NOT NULL ;
-ALTER TABLE `ad_per` ADD `last_edit` INT( 16 ) NOT NULL 
+CREATE TABLE `ad_vorwahlen` (
+  `v_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `vorwahl` varchar(20) COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`v_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=101 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
