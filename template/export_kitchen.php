a1142a63ca9dfc55e0ea6fd88a580ba4f441399b
Martin Ueding <dev@martin-ueding.de>
2012-02-12 14:51:20 +0100

    Whitespace

--- template/export_kitchen.php
+++ template/export_kitchen.php
@@ -11,7 +11,6 @@ $SCHRIFTGROESSE = 9;
 header("Content-Type: text/plain; charset=iso-8859-1");
 header('Content-Disposition: attachment; filename="adressen-'.time().'.tex"');
 
-
 echo '\documentclass[10pt]{article}
 \usepackage[left=7mm, right=7mm, top=7mm, bottom=7mm, scale=1, landscape]{geometry}
 \geometry{a4paper}
@@ -21,7 +20,6 @@ echo '\documentclass[10pt]{article}
 \renewcommand\sfdefault{phv}
 \renewcommand\familydefault{\sfdefault}
 ';
-
 
 echo '\begin{document}'."\n";
 
