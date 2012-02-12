fcd877d4e79a5c2be7dc71b956bd4c143a7004ed
Martin Ueding <dev@martin-ueding.de>
2012-02-12 15:16:48 +0100

    Start with database migration script

---
+++ install/database_migration_3.1.1_to_3.2.sql
@@ -0,0 +1,4 @@
+-- This migrates the database from the 3.1.1 scheme to the new 3.2 scheme.
+
+-- The previous scheme had no real pattern, German names and so on. The new
+-- scheme is compliant with the CakePHP database conventions.
