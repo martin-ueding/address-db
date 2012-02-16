b565839fc8ba5b53ae26c1f7893a2343ae70a013
Martin Ueding <dev@martin-ueding.de>
2012-02-16 22:55:49 +0100

    Convert into plain JS

---
+++ js/select_switch.js
@@ -0,0 +1,23 @@
+// Copyright © 2011-2012 Martin Ueding <dev@martin-ueding.de>
+
+// Adds a switch button to all input fields that consist of a regular input
+// field and a select field.
+
+$(initSwitchFields);
+
+function initSwitchFields() {
+	$("input.manual_area_code").hide(0).parent().children("input.manual_area_code").before("<img src=\'gfx/agt_reload.png\' class=\'switch_fields\' title=\''._('switch between a select and a manual input').'\' />");
+
+	$("img.switch_fields").click(function () {
+		if ($(this).parent().children("select").css("display") == "none") {
+			$(this).parent().children("input.manual_area_code").hide(100, function() {
+				$(this).parent().children("select").show(100);
+			}).val("");
+		}
+		else {
+			$(this).parent().children("select").hide(100, function () {
+				$(this).parent().children("input.manual_area_code").show(100);
+			}).val("1");
+		}
+	});
+}
--- template/index.php
+++ template/index.php
@@ -16,7 +16,7 @@
 		<script src="js/jquery.min.js"></script>
 		<script src="js/message_box.js"></script>
 		<script src="js/search.js"></script>
-		<script src="js/selectSwitch.js.php"></script>
+		<script src="js/select_switch.js"></script>
 		<script src="js/switch.js"></script>
 
 		<link rel="shortcut icon" type="image/x-icon" href="gfx/favicon.ico" />
