9f01875c1a075f0094d794de157ca7711c50303e
Martin Ueding <dev@martin-ueding.de>
2012-02-16 23:00:49 +0100

    Clean up install files

--- install/_config.inc.template.php
+++ install/_config.inc.template.php
@@ -1,27 +1,6 @@
 <?php
 # Copyright © 2011-2012 Martin Ueding <dev@martin-ueding.de>
 
-// Insert MySQL Data here
-$server = 'localhost';
-$user = 'root';
-$passwd = '';
-$db = 'addressdb';
-
-// URL to Server with tailing slash
-$url_to_server = 'http://example.com/addressdb/';
-
-$admin_name = '';
-$admin_email = 'example@example.com';
-
 $date_format = 'd.m.Y';
 $time_format = 'd.m.Y H:i:s';
-
-$path_to_phpmailer = '../PHPMailer_v2.0.4/class.phpmailer.php';
-
-// Do not change anything below this line please ---
-
-$dbh = mysql_connect($server, $user, $passwd);
-
-if ($dbh)
-	mysql_select_db($db, $dbh);
 ?>
--- install/database_migration_3.1.1_to_3.2.sql
+++
@@ -1,4 +0,0 @@
--- This migrates the database from the 3.1.1 scheme to the new 3.2 scheme.
-
--- The previous scheme had no real pattern, German names and so on. The new
--- scheme is compliant with the CakePHP database conventions.
