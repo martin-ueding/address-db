b7475f63cb60b556c9b426a53e2c1ebd88322779
Martin Ueding <dev@martin-ueding.de>
2012-02-16 22:10:23 +0100

    I18n

--- template/export_kitchen.php
+++ template/export_kitchen.php
@@ -53,37 +53,36 @@ while ($l = mysql_fetch_assoc($erg)) {
 		}
 	}
 
-	// TODO i18n
 	if (!empty($l['tel_privat']))
-		$content .= 'Tel Privat: '.AreaCode::select_vw_id($l['vw_privat_r']).'-'.$l['tel_privat'].Latex::bruch();
+		$content .= _('Tel Privat').': '.AreaCode::select_vw_id($l['vw_privat_r']).'-'.$l['tel_privat'].Latex::bruch();
 
 	if (!empty($l['tel_arbeit']))
-		$content .= 'Tel Arbeit: '.AreaCode::select_vw_id($l['vw_arbeit_r']).'-'.$l['tel_arbeit'].Latex::bruch();
+		$content .= _('Tel Arbeit').': '.AreaCode::select_vw_id($l['vw_arbeit_r']).'-'.$l['tel_arbeit'].Latex::bruch();
 
 	if (!empty($l['tel_mobil']))
-		$content .= 'Handy: '.AreaCode::select_vw_id($l['vw_mobil_r']).'-'.$l['tel_mobil'].Latex::bruch();
+		$content .= _('Handy').': '.AreaCode::select_vw_id($l['vw_mobil_r']).'-'.$l['tel_mobil'].Latex::bruch();
 
 	if (!empty($l['tel_fax']))
-		$content .= 'Fax: '.AreaCode::select_vw_id($l['vw_fax_r']).'-'.$l['tel_fax'].Latex::bruch();
+		$content .= _('Fax').': '.AreaCode::select_vw_id($l['vw_fax_r']).'-'.$l['tel_fax'].Latex::bruch();
 
 	if (!empty($l['tel_aux']))
-		$content .= 'Tel: '.AreaCode::select_vw_id($l['vw_aux_r']).'-'.$l['tel_aux'].Latex::bruch();
+		$content .= _('Tel').': '.AreaCode::select_vw_id($l['vw_aux_r']).'-'.$l['tel_aux'].Latex::bruch();
 
 	if (isset($last) && $last['adresse_r'] != $l['adresse_r']) {
 		if (!empty($l['ftel_privat']))
-			$content .= 'Tel Privat: '.AreaCode::select_vw_id($l['fvw_privat_r']).'-'.$l['ftel_privat'].Latex::bruch();
+			$content .= _('Tel Privat').': '.AreaCode::select_vw_id($l['fvw_privat_r']).'-'.$l['ftel_privat'].Latex::bruch();
 
 		if (!empty($l['ftel_arbeit']))
-			$content .= 'Tel Arbeit: '.AreaCode::select_vw_id($l['fvw_arbeit_r']).'-'.$l['ftel_arbeit'].Latex::bruch();
+			$content .= _('Tel Arbeit').': '.AreaCode::select_vw_id($l['fvw_arbeit_r']).'-'.$l['ftel_arbeit'].Latex::bruch();
 
 		if (!empty($l['ftel_mobil']))
-			$content .= 'Handy: '.AreaCode::select_vw_id($l['fvw_mobil_r']).'-'.$l['ftel_mobil'].Latex::bruch();
+			$content .= _('Handy').': '.AreaCode::select_vw_id($l['fvw_mobil_r']).'-'.$l['ftel_mobil'].Latex::bruch();
 
 		if (!empty($l['ftel_fax']))
-			$content .= 'Fax: '.AreaCode::select_vw_id($l['fvw_fax_r']).'-'.$l['ftel_fax'].Latex::bruch();
+			$content .= _('Fax').': '.AreaCode::select_vw_id($l['fvw_fax_r']).'-'.$l['ftel_fax'].Latex::bruch();
 
 		if (!empty($l['ftel_aux']))
-			$content .= 'Tel: '.AreaCode::select_vw_id($l['fvw_aux_r']).'-'.$l['ftel_aux'].Latex::bruch();
+			$content .= _('Tel').': '.AreaCode::select_vw_id($l['fvw_aux_r']).'-'.$l['ftel_aux'].Latex::bruch();
 	}
 
 
