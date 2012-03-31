b733e1c56e1e1e80300957eca20c86ef655b4349
Martin Ueding <dev@martin-ueding.de>
2012-03-31 16:34:16 +0200

    Show mysql errors

--- dump.php
+++ dump.php
@@ -6,16 +6,19 @@
 require_once('component/Login.php');
 
 $tables = mysql_query("SHOW TABLES FROM ".$db_login->connection['database']);
+echo mysql_error();
 while ($cells = mysql_fetch_array($tables)) {
 	$table = $cells[0];
 	echo "DROP TABLE IF EXISTS `$table`;\n";
 	$res = mysql_query("SHOW CREATE TABLE `$table`");
+	echo mysql_error();
 	if ($res) {
 		$create = mysql_fetch_array($res);
 		$create[1] .= ";";
 		$line = str_replace("\n", "", $create[1]);
 		echo $line;
 		$data = mysql_query("SELECT * FROM `$table`");
+		echo mysql_error();
 		$num = mysql_num_fields($data);
 		while ($row = mysql_fetch_array($data)) {
 			$line = "INSERT INTO `$table` VALUES(";
